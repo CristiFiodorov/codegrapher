ee60a367e09b7ff25d0eb76ed73bfc713020ed9c
Fiodorov Cristian <cristifv59@gmail.com>
2026-03-19 00:24:12 +0200

    added BaseDriver

---
+++ src/tree_parser/base_driver.py
@@ -0,0 +1,40 @@
+from abc import ABC, abstractmethod
+import networkx as nx
+from tree_sitter import Language, Parser
+
+from utils.preprocessor import remove_comments, remove_empty_lines
+
+
+class BaseDriver(ABC):
+    def __init__(self, language: Language, lang_name: str):
+        self._language = language
+        self._lang_name = lang_name
+        self._parser = Parser()
+        self._parser.set_language(language)
+        self._graph: nx.DiGraph = nx.DiGraph()
+        self._counter = 0
+
+    def _next_id(self):
+        nid = self._counter
+        self._counter += 1
+        return nid
+
+    def parse(self, source: str, preprocess: bool = True):
+        self._graph = nx.DiGraph()
+        self._counter = 0
+
+        if preprocess:
+            source = remove_comments(self._lang_name, source)
+            source = remove_empty_lines(source)
+
+        tree = self._parser.parse(bytes(source, "utf-8"))
+        self._build_graph(tree.root_node)
+
+        return self._graph
+
+    @abstractmethod
+    def _build_graph(self, tree_root):
+        """
+        This function should be implemented be subclasses to build graph
+        """
+        ...
