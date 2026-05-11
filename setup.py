194026d4c6a1105eab6c4880075f752e31fe3732
Cristian FIODOROV <cristifv59@gmail.com>
2026-05-11 13:20:31 +0300

    added setup.py

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="codegrapher",
+    version="0.1.0",
+    description="Parse source code into AST, DFG or combined graphs",
+    package_dir={"": "src"},
+    packages=find_packages(where="src"),
+    python_requires=">=3.8",
+    install_requires=[
+        "tree-sitter>=0.24",
+        "tree-sitter-language-pack>=0.7",
+        "networkx",
+    ],
+    entry_points={
+        "console_scripts": [
+            "codegrapher=main:main",
+        ],
+    },
+)
